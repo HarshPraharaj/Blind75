ff85b68888756723827233b3cb584dd46ff11dcb
Harsh Praharaj <praharajhp@gmail.com>
2023-07-04 20:42:38 -0700

    Create 141_LinkedListCycle.py

---
+++ 141_LinkedListCycle.py
@@ -0,0 +1,21 @@
+## Floyd Tortoise and hare algorithm --> slow and fast pointers -- if they ever meet, then loop exists
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
